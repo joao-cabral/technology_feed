d8c87a4f9e8301e3a198ad43412dfe605786ec3b
João Cabral <joaocabral1232@gmail.com>
2022-10-19 23:17:21 -0400

    fix: simplified response model

--- lib/model/subreddit_model.dart
+++ lib/model/subreddit_model.dart
@@ -1,64 +1,25 @@
-class ResponseModel {
-  ResponseModel({
-    this.data,
+class PostsModel {
+  PostsModel({
+    this.posts,
   });
 
-  String? kind;
-  ResponseModelData? data;
+  List<PostModel>? posts;
 
-  factory ResponseModel.fromJson(Map<String, dynamic> json) => ResponseModel(
-        data: ResponseModelData.fromJson(json["data"]),
+  factory PostsModel.fromJson(Map<String, dynamic> json) => PostsModel(
+        posts: List<PostModel>.from(
+          json["data"]["children"].map(
+            (post) => PostModel.fromJson(post),
+          ),
+        ),
       );
 }
 
-class ResponseModelData {
-  ResponseModelData({
-    this.after,
-    required this.dist,
-    this.modhash,
-    this.geoFilter,
-    this.children,
-    this.before,
-  });
-
-  dynamic after;
-  int dist;
-  String? modhash;
-  String? geoFilter;
-  List<Child>? children;
-  String? before;
-
-  factory ResponseModelData.fromJson(Map<String, dynamic> json) =>
-      ResponseModelData(
-        after: json["after"],
-        dist: json["dist"],
-        modhash: json["modhash"],
-        geoFilter: json["geo_filter"],
-        children:
-            List<Child>.from(json["children"].map((x) => Child.fromJson(x))),
-        before: json["before"],
-      );
-}
-
-class Child {
-  Child({
-    this.data,
-  });
-
-  ChildData? data;
-
-  factory Child.fromJson(Map<String, dynamic> json) => Child(
-        data: ChildData.fromJson(json["data"]),
-      );
-}
-
-class ChildData {
-  ChildData({
+class PostModel {
+  PostModel({
     this.title,
     this.name,
     this.score,
     this.thumbnail,
-    this.likes,
     this.numComments,
     this.author,
     this.url,
@@ -69,21 +30,19 @@ class ChildData {
   String? name;
   int? score;
   String? thumbnail;
-  dynamic likes;
   int? numComments;
   String? author;
   String? url;
   String? permalink;
 
-  factory ChildData.fromJson(Map<String, dynamic> json) => ChildData(
-        title: json["title"],
-        name: json["name"],
-        score: json["score"],
-        thumbnail: json["thumbnail"],
-        likes: json["likes"],
-        numComments: json["num_comments"],
-        author: json["author"],
-        url: json["url"],
-        permalink: json["permalink"],
+  factory PostModel.fromJson(Map<String, dynamic> json) => PostModel(
+        title: json["data"]["title"],
+        name: json["data"]["name"],
+        score: json["data"]["score"],
+        thumbnail: json["data"]["thumbnail"],
+        numComments: json["data"]["num_comments"],
+        author: json["data"]["author"],
+        url: json["data"]["url"],
+        permalink: json["data"]["permalink"],
       );
 }
